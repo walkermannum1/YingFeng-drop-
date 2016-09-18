ecde25d1399801db62b742ec4c51aa1e9cfd5532
walkermannum1 <2469377884@qq.com>
2016-09-18 20:07:29 +0800

    Something wrong with SO file.

--- app/src/main/java/com/example/user/yingfeng/MapActivity.java
+++ app/src/main/java/com/example/user/yingfeng/MapActivity.java
@@ -3,11 +3,34 @@ package com.example.user.yingfeng;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
 public class MapActivity extends AppCompatActivity {
+
+    MapView mMapView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_map);
+        mMapView = (MapView) findViewById(R.id.map_view);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
     }
 }
